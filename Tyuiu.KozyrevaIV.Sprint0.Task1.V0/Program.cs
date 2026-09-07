7243f084057c4f90aab4b4e723d9b19e18698d78
Kozyreva Irina <KozyrevaIV>
2026-09-07 21:31:20 +0500

    Task1

---
+++ Tyuiu.KozyrevaIV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Tyuiu.KozyrevaIV.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Ирина";
+            int age = 17;
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+        }
+    }
+}
