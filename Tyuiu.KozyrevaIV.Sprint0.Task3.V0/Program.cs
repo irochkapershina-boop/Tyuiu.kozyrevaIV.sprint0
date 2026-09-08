fb16aca389e310c2d0998abacb08856849e4f662
Kozyreva Irina <KozyrevaIV>
2026-09-08 20:14:28 +0500

    Task3

---
+++ Tyuiu.KozyrevaIV.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.KozyrevaIV.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.KozyrevaIV.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+        }
+    }
+}
