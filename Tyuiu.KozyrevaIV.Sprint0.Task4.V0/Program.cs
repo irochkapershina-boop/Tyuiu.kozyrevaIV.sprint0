ed266c6c730be729a3ed80da95c847c64543c8e9
Kozyreva Irina <KozyrevaIV>
2026-09-09 21:50:06 +0500

    Task4

---
+++ Tyuiu.KozyrevaIV.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KozyrevaIV.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.KozyrevaIV.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(7, 5));
+            Console.WriteLine(DataService.Subtraction(7, 5));
+            Console.WriteLine(DataService.Multiplication(7, 5));
+            Console.WriteLine(DataService.Division(20, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
